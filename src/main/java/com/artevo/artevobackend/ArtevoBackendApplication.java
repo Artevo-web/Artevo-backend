04caa735131eeee587d1ce230ed6a11d25808af3
artevo <artevo.artwear@outlook.de>
2025-08-19 22:44:41 +0200

    first commit

---
+++ src/main/java/com/artevo/artevobackend/ArtevoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.artevo.artevobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtevoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtevoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artevo/artevobackend/ArtevoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artevo.artevobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtevoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
